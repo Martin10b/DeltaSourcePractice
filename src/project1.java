c560117e471b1acccb78f696c49a7eb8ab5667d7
User <martinmarinov766@gmail.com>
2022-07-05 13:14:18 +0300

    homework

---
+++ src/project1.java
@@ -0,0 +1,15 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+    public class project1 {
+        public static void main(String[] args) {
+            DecimalFormat df = new DecimalFormat("#.##");
+            Scanner scanner = new Scanner(System.in);
+
+            double USD = Double.parseDouble(scanner.nextLine());
+            double BGN = USD * 1.79549 ;
+
+            System.out.println(df.format(BGN) + " BGN");
+        }
+    }
+
