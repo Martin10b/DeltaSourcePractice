b88003f69697eff9054a45963b0a5cda3c9dd6dd
User <martinmarinov766@gmail.com>
2022-07-08 10:12:16 +0300

    srednoaretmetichno na sumata ot elementite

--- src/jul8ex1sum_of_elements_in_array.java
+++ src/jul8ex1sum_of_elements_in_array.java
@@ -1,11 +1,11 @@
-public class jul8ex1 {
+public class jul8ex1sum_of_elements_in_array {
 
     public static void main(String[] args) {
 
         int[] arr = new int[] {10,20,30,40,50,60,70,80,90,100};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
+            sum = sum + arr[i] / 10;
         }
         System.out.println("Sum of all the elements of an array: " + sum);
     }
