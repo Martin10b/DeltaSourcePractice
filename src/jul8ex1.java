35b01bf61883e187bef4cbee11065b4811108c35
User <martinmarinov766@gmail.com>
2022-07-08 10:02:08 +0300

    sum of elements

--- src/jul8ex1.java
+++ src/jul8ex1.java
@@ -1,5 +1,7 @@
 public class jul8ex1 {
+
     public static void main(String[] args) {
+
         int[] arr = new int[] {10,20,30,40,50,60,70,80,90,100};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
