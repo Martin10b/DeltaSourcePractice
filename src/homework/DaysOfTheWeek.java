be664353de3d283ab0a2d03fb9909521c49cd7cf
User <martinmarinov766@gmail.com>
2022-07-08 09:10:29 +0300

    stilqn e shopar

--- src/homework/DaysOfTheWeek.java
+++ src/homework/DaysOfTheWeek.java
@@ -1,36 +1,41 @@
 package homework;
-import java.util. Scanner;
-    public class DaysOfTheWeek{
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int input = sc.nextInt();
-            switch (input) {
-                case 1:
-                    System.out.println("Monday");
-                    break;
-                case 2:
-                    System.out.println("Tuesday");
-                    break;
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                case 6:
-                    System.out.println("Saturday");
-                    break;
-                case 7:
-                    System.out.println("Sunday");
-                    break;
-                default:
-                    System.out.println("Number is out of the 1-7 range.");
-            }
+
+import java.util.Scanner;
+
+public class DaysOfTheWeek {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int dayOfTheWeek = scanner.nextInt();
+
+        switch (dayOfTheWeek) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Number is out of the 1-7 range.");
         }
     }
+}
 
 
 
