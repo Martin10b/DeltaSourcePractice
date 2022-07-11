6a3f62ab137de6e793194790dddca565e6cc005c
User <martinmarinov766@gmail.com>
2022-07-11 13:02:21 +0300

    bubbleSort

--- src/classWork/bubblesort.java
+++ src/classWork/bubblesort.java
@@ -1,13 +1,12 @@
 package classWork;
 
-public class bubblesort {
-    static void bubbleSort(int[] arr) {
+public class bubbleSort {
+    static void BubbleSort(int[] arr) {
         int n = arr.length;
         int temp = 0;
         for(int i=0; i < n; i++){
             for(int j=1; j < (n-i); j++){
                 if(arr[j-1] > arr[j]){
-                    //swap elements
                     temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
@@ -26,7 +25,7 @@ public class bubblesort {
         }
         System.out.println();
 
-        bubbleSort(arr);//sorting array elements using bubble sort
+        BubbleSort(arr);
 
         System.out.println("Array After Bubble Sort");
         for(int i=0; i < arr.length; i++){
