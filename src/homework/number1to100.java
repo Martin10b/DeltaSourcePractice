1c9d78e28c101041f5140479785fbf51077fad2b
User <martinmarinov766@gmail.com>
2022-07-11 09:11:49 +0300

    number1to100

---
+++ src/homework/number1to100.java
@@ -0,0 +1,28 @@
+package homework;
+
+import java.util.Scanner;
+
+public class number1to100 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int number = scan.nextInt();
+
+        if (number > 0 && number <= 100) {
+
+            System.out.println("Your number is: " + number);
+
+
+        } else {
+
+            System.out.println("Invalid Number!");
+
+        }
+
+    }
+
+
+}
