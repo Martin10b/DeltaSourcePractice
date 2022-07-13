0f8e3c99aabbb754e8aa15c062747660ca00cd82
User <martinmarinov766@gmail.com>
2022-07-13 09:38:52 +0300

    racers

--- src/homework/Racers.java
+++ src/homework/Racers.java
@@ -2,7 +2,7 @@ package homework;
 
 import java.util. Scanner;
 
-public class sum_of_seconds {
+public class Racers {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
